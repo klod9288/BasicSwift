65c7c3d9ebf2ee1a4ecb6e459fb6014b02a476d3
srongklod <klod9288@gmail.com>
2018-12-17 10:37:31 +0700

    Show Console

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,3 +3,21 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Explicit คือ การประกาศตัวแปร แบบ กำหนด Data Type
+var name: String = "Doramon"
+var number1: Int = 123
+var number2: Double = 12.34
+var status:Bool = true
+
+//Show Console
+print("This is Swift")
+print("nameString ==>"+name)//Java
+print("nameString ==> \(name)")//Swift
+print("number1 ==>\(number1)")
+
+
+
+
+
+
